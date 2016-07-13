ddb58a6c62c186ab4b906f8520ee6d50e865d8ed
lambda <fluss.der.trauer@gmail.com>
2016-07-13 06:26:21 +0300

    Move index page render method from view to url

--- src/core/urls.py
+++ src/core/urls.py
@@ -4,14 +4,13 @@ from __future__ import unicode_literals
 from allauth.account import views
 from django.conf.urls import url, include
 from django.contrib import admin
-
-from src.core.views import IndexView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', views.login, name='account_login'),
     url(r'^logout/$', views.logout, name='account_logout'),
     url(r'^signup/$', views.signup, name='account_signup'),
-    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^$', TemplateView.as_view(template_name='pages/index.html'), name='index'),
     url(r'^products/', include('src.apps.product.urls', namespace='products'))
 ]
